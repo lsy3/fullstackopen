f4c539fde3964763076e923eeeedb57329b4a9f1
lsy3 <sylukewicent@gmail.com>
2022-08-31 02:29:46 +1000

    Added solution to Part 8 Ex 8.1-8.12

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
+
+const Authors = (props) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const result = useQuery(ALL_AUTHORS)
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }]
+  })
+
+  if (!props.show || result.loading) {
+    return null
+  }
+
+  const authors = result.data.allAuthors
+
+  const onSetBirthyear = (event) => {
+    event.preventDefault()
+
+    const bornN = Number(born)
+    editAuthor({ variables: { name, setBornTo: bornN } })
+
+    setBorn('')
+  }
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h2>Set birthyear</h2>
+      <form onSubmit={onSetBirthyear}>
+        <div>
+          name
+          <select value={name} onChange={({ target }) => setName(target.value)}>
+            {
+              authors.map((a) => (
+                <option key={a.name} value={a.name}>{a.name}</option>
+              ))
+            }
+          </select>
+        </div>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default Authors
